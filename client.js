aee70baa9ee286884cf20a51cfd2602ced573239
Radzik69 <126457866+Radzik69@users.noreply.github.com>
2023-03-19 22:02:32 +0100

    koks skomnczone

---
+++ client.js
@@ -0,0 +1,114 @@
+var wybranyKandydat = ""
+
+function kandydatWybrany() {
+    var kandydat = document.createElement("input")
+    kandydat.setAttribute("id", "kandydat")
+    kandydat.setAttribute("type", "hidden")
+    kandydat.setAttribute("value", wybranyKandydat)
+    kandydat.setAttribute("name", "kandydat")
+    glosowanie.appendChild(kandydat)
+
+    var pesel = document.createElement("input")
+    pesel.setAttribute("id", "pesel")
+    pesel.setAttribute("type", "text")
+    pesel.setAttribute("placeholder", "pesel")
+    pesel.setAttribute("name", "pesel")
+    pesel.setAttribute("onchange", "spelnionewymagania()")
+    pesel.setAttribute("maxlength", "11")
+    glosowanie.appendChild(pesel)
+
+    var imie = document.createElement("input")
+    imie.setAttribute("id", "imie")
+    imie.setAttribute("type", "text")
+    imie.setAttribute("placeholder", "imie")
+    imie.setAttribute("name", "imie")
+    imie.setAttribute("onchange", "spelnionewymagania()")
+    imie.setAttribute("maxlength", "20")
+    glosowanie.appendChild(imie)
+
+    var nazwisko = document.createElement("input")
+    nazwisko.setAttribute("id", "nazwisko")
+    nazwisko.setAttribute("type", "text")
+    nazwisko.setAttribute("placeholder", "nazwisko")
+    nazwisko.setAttribute("name", "nazwisko")
+    nazwisko.setAttribute("onchange", "spelnionewymagania()")
+    nazwisko.setAttribute("maxlength", "30")
+    glosowanie.appendChild(nazwisko)
+
+    var wiek = document.createElement("input")
+    wiek.setAttribute("id", "wiek")
+    wiek.setAttribute("type", "text")
+    wiek.setAttribute("placeholder", "wiek")
+    wiek.setAttribute("name", "wiek")
+    wiek.setAttribute("onchange", "spelnionewymagania()")
+    wiek.setAttribute("maxlength", "2")
+    glosowanie.appendChild(wiek)
+}
+
+function spelnionewymagania() {
+    var pesel = document.getElementById("pesel")
+    var imie = document.getElementById("imie")
+    var nazwisko = document.getElementById("nazwisko")
+    var wiek = document.getElementById("wiek")
+    var glosowanie = document.getElementById("glosowanie")
+
+    if (pesel.value != "" && imie.value != "" && nazwisko.value != "" && wiek.value != ""
+    ) {
+        var form = document.getElementById("formularz");
+        var submit = document.createElement("input")
+        submit.setAttribute("id", "send")
+        submit.setAttribute("type", "submit")
+        submit.value = "Głosuj"
+        submit.addEventListener("click", function () {
+            form.submit()
+            zaglosowano()
+        });
+        glosowanie.appendChild(submit)
+    }
+}
+function zaglosowano() {
+    const body = document.getElementById("body")
+    body.innerHTML = ""
+    body.style.backgroundColor = "gray"
+    var napis = document.createElement("h1")
+    napis.setAttribute("id", "napis")
+    napis.innerHTML = "ZAGŁOSOWANO"
+    // wykresLink()
+    body.appendChild(napis)
+}
+
+function wybor1() {
+    wybranyKandydat = "Kandydant nr 1"
+    document.getElementById("kandydaci").innerHTML = ""
+    var info = document.createElement("h1")
+    info.setAttribute("id", "info")
+    info.innerHTML = "Zagłosowałeś na kandydata nr 1"
+    kandydaci.appendChild(info)
+    console.log(wybranyKandydat)
+}
+function wybor2() {
+    wybranyKandydat = "Kandydant nr 2"
+    document.getElementById("kandydaci").innerHTML = ""
+    var info = document.createElement("h1")
+    info.setAttribute("id", "info")
+    info.innerHTML = "Zagłosowałeś na kandydata nr 2"
+    kandydaci.appendChild(info)
+    console.log(wybranyKandydat)
+}
+function wybor3() {
+    wybranyKandydat = "Kandydant nr 3"
+    document.getElementById("kandydaci").innerHTML = ""
+    var info = document.createElement("h1")
+    info.setAttribute("id", "info")
+    info.innerHTML = "Zagłosowałeś na kandydata nr 3"
+    kandydaci.appendChild(info)
+    console.log(wybranyKandydat)
+}
+
+function wykresLink() {
+    var info = document.createElement("a")
+    info.href= '/wykres'
+    info.innerText = "Zobacz wykres"
+    body.appendChild(info)
+    console.log('pokaz wykres')
+}
---
+++ server.js
@@ -0,0 +1,81 @@
+//nazwa bazy danych:glosowanie      nazwa tabeli:glosujacy:   w srodku:     pesel:int(11znakow)     imie:text(20znakow)     nazwisko:text(30znakow)     wiek:int(3znaki)       kandydat
+
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql')
+const chart = require('chart.js')
+const app = express()
+const PORT = 3000
+app.use(cors())
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  passworld: "",
+  database: "glosowanie",
+})
+
+con.connect(function (err) {
+  if (err) console.log(err)
+  else console.log("poloczono z baza danych")
+})
+
+app.use(cors())
+app.use(express.static(__dirname))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM glosujacy"
+  con.query(sql, function (err, result, fields) {
+    if (err) console.log(err)
+    else res.send(result)
+  })
+})
+
+app.get("/add", (req, res) => {
+  var { pesel, imie, nazwisko, wiek, kandydat } = req.query
+  console.log(' pesel:', `${req.query.pesel}`)
+  console.log(' imie:', `${req.query.imie}`)
+  console.log(' nazwisko:', `${req.query.nazwisko}`)
+  console.log(' wiek:', `${req.query.wiek}`)
+  console.log(' kandydat:', `${req.query.kandydat}`)
+  const sql = `INSERT INTO glosujacy(pesel,imie,nazwisko,wiek,kandydat) VALUES( '${pesel}','${imie}','${nazwisko}','${wiek}','${kandydat}')`
+  console.log('------------')
+  con.query(sql)
+})
+
+app.get("/getdata1", (req, res) => {
+  const sql = 'SELECT count(kandydat) AS "liczba" FROM glosujacy where kandydat="Kandydant nr 1"'
+  con.query(sql,function(err,result,fields){
+      if(err) console.log(err)
+      else res.send(`${result[0].liczba};`)
+  })
+})
+
+app.get("/getdata2", (req, res) => {
+  const sql = 'SELECT count(kandydat) AS "liczba" FROM glosujacy where kandydat="Kandydant nr 2"'
+  con.query(sql,function(err,result,fields){
+      if(err) console.log(err)
+      else res.send(`${result[0].liczba};`)
+  })
+})
+
+app.get("/getdata3", (req, res) => {
+  const sql = 'SELECT count(kandydat) AS "liczba" FROM glosujacy where kandydat="Kandydant nr 3"'
+  con.query(sql,function(err,result,fields){
+      if(err) console.log(err)
+      else res.send(`${result[0].liczba};`)
+  })
+})
+
+app.get("/wykres", (req, res) => {
+  console.log("wykres")
+  res.sendFile(__dirname + "/wykres.html")
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
